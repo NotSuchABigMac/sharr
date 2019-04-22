3c26b2f69c7a0ec930a9caffc6e88989ccae8697
Alex Tolnai <s3540645@student.rmit.edu.au>
2019-04-22 17:39:28 +1000

    graph files renamed

--- src/components/InteractiveCandlestick.vue
+++ src/components/InteractiveCandlestick.vue
@@ -1,231 +1,104 @@
 <template>
-  <div class="highlights-item col-md-4 col-sm-6 col-xs-12 border-top">
-    <div>
-      <fusioncharts
-        :type="type"
-        :width="width"
-        :height="height"
-        :containerbackgroundopacity="containerbackgroundopacity"
-        :dataformat="dataformat"
-        :datasource="datasource"
-      ></fusioncharts>
+    <div class="custom-card header-card card">
+        <div class="card-body pt-0">
+            <fusioncharts
+                    :type="type"
+                    :width="width"
+                    :height="height"
+                    :dataFormat="dataFormat"
+                    dataEmptyMessage="i-https://i.postimg.cc/R0QCk9vV/Rolling-0-9s-99px.gif"
+                    dataEmptyMessageImageScale=39
+                    :dataSource="dataSource"
+            ></fusioncharts>
+        </div>
+
     </div>
-  </div>
 </template>
 
-<script>
-export default {
-  props: ["highlights"],
-  components: {},
-  data() {
-    return {
-      type: "inversemsline",
-      width: "100%",
-      height: "400",
-      containerbackgroundopacity: 0,
-      dataformat: "json",
-      datasource: {
-        chart: {
-          caption: "FIFA Ranking",
-          subcaption: "Argentina vs Germany<br>2000-2018",
-          theme: "fusion",
-          yaxisname: "Rank",
-          xaxisname: "Years",
-          plottooltext: "$seriesName: <b>$dataValue</b>",
-          rotatelabels: "1",
-          slantlabels: "0"
+    <script>
+        import FusionCharts from "fusioncharts";
+       /* function printObject(o) {
+            var out = '';
+            for (var p in o) {
+                out += p + ': ' + o[p] + '\n';
+            }
+            alert("alert Out" + out.toString());
+        }*/
+        var jsonify = res => res.json();
+        var dataFetch = fetch(
+            "https://s3.eu-central-1.amazonaws.com/fusion.store/ft/data/candlestick-chart-data.json"
+        ).then(jsonify);
+        var schemaFetch = fetch(
+            "https://s3.eu-central-1.amazonaws.com/fusion.store/ft/schema/candlestick-chart-schema.json"
+        ).then(jsonify);
+        //printObject(schemaFetch);
+        ///alert("alert Out2" + schemaFetch.toString());
+
+    export default {
+        props: ["StockGraph"],
+        components: {},
+        data: function() {
+return {
+            width: "100%",
+            height: "600",
+            type: "timeseries",
+            dataFormat: "json",
+            dataSource: {
+                chart: {theme: "gammel",},
+                data: null,
+                caption: {
+                    text: "Apple Inc. Stock Price"
+                },
+                subcaption: {
+                    text: "Stock prices from January 1980 - November 2011"
+                },
+                yaxis: [
+                    {
+                        plot: {
+                            value: {
+                                open: "Open",
+                                high: "High",
+                                low: "Low",
+                                close: "Close"
+                            },
+                            type: "candlestick"
+                        },
+                        format: {
+                            prefix: "$"
+                        },
+                        title: "Stock Value"
+                    }
+                ]
+            }
+        };
+},
+        methods: {
+            setChartData: function() {
+                Promise.all([dataFetch, schemaFetch]).then(res => {
+                    const data = res[0];
+                    const schema = res[1];
+                    const fusionTable = new FusionCharts.DataStore().createDataTable(
+                        data,
+                        schema
+                    );
+                    this.dataSource.data = fusionTable;
+                    //alert("alert Out2.5:" + fusionTable.toString());
+                });
+            },
         },
-        categories: [
-          {
-            category: [
-              {
-                label: "2000"
-              },
-              {
-                label: "2001"
-              },
-              {
-                label: "2002"
-              },
-              {
-                label: "2003"
-              },
-              {
-                label: "2004"
-              },
-              {
-                label: "2005"
-              },
-              {
-                label: "2006"
-              },
-              {
-                label: "2007"
-              },
-              {
-                label: "2008"
-              },
-              {
-                label: "2009"
-              },
-              {
-                label: "2010"
-              },
-              {
-                label: "2011"
-              },
-              {
-                label: "2012"
-              },
-              {
-                label: "2013"
-              },
-              {
-                label: "2014"
-              },
-              {
-                label: "2015"
-              },
-              {
-                label: "2016"
-              },
-              {
-                label: "2017"
-              },
-              {
-                label: "2018"
-              }
-            ]
-          }
-        ],
-        dataset: [
-          {
-            seriesname: "Germany",
-            data: [
-              {
-                value: "11"
-              },
-              {
-                value: "12"
-              },
-              {
-                value: "4"
-              },
-              {
-                value: "12"
-              },
-              {
-                value: "19"
-              },
-              {
-                value: "16"
-              },
-              {
-                value: "6"
-              },
-              {
-                value: "5"
-              },
-              {
-                value: "2"
-              },
-              {
-                value: "6"
-              },
-              {
-                value: "3"
-              },
-              {
-                value: "3"
-              },
-              {
-                value: "2"
-              },
-              {
-                value: "2"
-              },
-              {
-                value: "1"
-              },
-              {
-                value: "4"
-              },
-              {
-                value: "3"
-              },
-              {
-                value: "1"
-              },
-              {
-                value: "1"
-              }
-            ]
-          },
-          {
-            seriesname: "Argentina",
-            data: [
-              {
-                value: "3"
-              },
-              {
-                value: "2"
-              },
-              {
-                value: "5"
-              },
-              {
-                value: "5"
-              },
-              {
-                value: "3"
-              },
-              {
-                value: "4"
-              },
-              {
-                value: "3"
-              },
-              {
-                value: "1"
-              },
-              {
-                value: "6"
-              },
-              {
-                value: "8"
-              },
-              {
-                value: "5"
-              },
-              {
-                value: "10"
-              },
-              {
-                value: "3"
-              },
-              {
-                value: "3"
-              },
-              {
-                value: "2"
-              },
-              {
-                value: "2"
-              },
-              {
-                value: "1"
-              },
-              {
-                value: "4"
-              },
-              {
-                value: "5"
-              }
-            ]
-          }
-        ]
-      },
-    };
-  },
-};
+        mounted: function() {
+            this.setChartData();
+        },
+        watch: {
+            tempVar: {
+                handler: function() {
+                    this.setChartData();
+                },
+                deep: true
+            },
+        },
+            };
 </script>
+
+<style>
+</style>
