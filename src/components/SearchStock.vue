addc18ab83137d8a4f2665066e066c94bc4425d1
James McIntyre <jamesnpmcintyre@gmail.com>
2019-05-05 19:44:32 +1000

    Continued work on Searchstock

--- src/components/SearchStock.vue
+++ src/components/SearchStock.vue
@@ -18,20 +18,45 @@
           here are your results:
         </div>
         <div class=results id=result1>
-          <div class=stock-title>
-            First Result
+          <div class=firstline>
+              <div class=live-price>
+                  204.53
+              </div>
+              <div class=price-change>
+                  USD 0.00 (0.00%)
+              </div>
           </div>
-          <div class>
+          <div class=price-details>
+              Closed: 23 Apr, 8:46 am GMT-4 - Disclaimer <br>
+              Pre-market 204.10 -0.43 (0.21%)
           </div>
         </div>
         <div class=results id=result2>
-          <div class=stock-title>
-            Second Result
+          <div class=firstline>
+              <div class=live-price>
+                  204.53
+              </div>
+              <div class=price-change>
+                  USD 0.00 (0.00%)
+              </div>
+          </div>
+          <div class=price-details>
+              Closed: 23 Apr, 8:46 am GMT-4 - Disclaimer <br>
+              Pre-market 204.10 -0.43 (0.21%)
           </div>
         </div>
         <div class=results id=result3>
-          <div class=stock-title>
-            Third Result
+          <div class=firstline>
+              <div class=live-price>
+                  204.53
+              </div>
+              <div class=price-change>
+                  USD 0.00 (0.00%)
+              </div>
+          </div>
+          <div class=price-details>
+              Closed: 23 Apr, 8:46 am GMT-4 - Disclaimer <br>
+              Pre-market 204.10 -0.43 (0.21%)
           </div>
         </div>
       </div>
