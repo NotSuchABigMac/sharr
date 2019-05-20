c4edfb918da54efe25488e9da7f783fa8d9b4ed5
MinuraIddamalgoda <mini2476@gmail.com>
2019-05-21 08:28:38 +1000

    Added basic sign in and out flow

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="homepage">
+    <div class="auth">
+      <amplify-authenticator></amplify-authenticator>
+      <amplify-sign-out v-if="signedIn"></amplify-sign-out>
+    </div>
     <br>
     sharr
     <br>
@@ -12,13 +16,42 @@
 </template>
 
 <script>
+import Amplify, { Auth } from "aws-amplify";
+import { AmplifyEventBus } from 'aws-amplify-vue';
+
 export default {
+  data() {
+    return {
+      signedIn: false
+    }
+  },
   props: ["Home"],
   components: {},
+  created() {
+    this.isUserSignedIn();
+    AmplifyEventBus.$on('authState', info => {
+      if(info === 'signedIn') {
+        this.isUserSignedIn();
+      } else {
+        this.signedIn = false;
+      }
+    });
+  },
   methods: {
     setChartData: function() {
 
     },
+    async isUserSignedIn() {
+      try {
+        const userObj = await Auth.currentAuthenticatedUser();
+        // If it gets to here, then the user is signed in
+        this.signedIn = true;
+        console.log(userObj);
+      } catch(err) {
+        this.signedIn = false;
+        console.log(err);
+      }
+    }
   },
   mounted: function() {
     this.setChartData();
