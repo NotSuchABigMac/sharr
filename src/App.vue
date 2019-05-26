cc3a4c89c84e24da897989eed29293e04b560d51
James McIntyre <jamesnpmcintyre@gmail.com>
2019-05-26 23:19:49 +1000

    Removed error suppressions.

--- src/App.vue
+++ src/App.vue
@@ -21,9 +21,7 @@
 </template>
 
 <script>
-    // eslint-disable-next-line
     import { components } from 'aws-amplify-vue';
-    // eslint-disable-next-line
     import { AmplifyEventBus } from 'aws-amplify-vue';
 
     export default {
